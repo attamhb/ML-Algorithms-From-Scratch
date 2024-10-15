416b3e06070ae35c9aeb1872a7175c1daec2a3b3
Atta Ullah <atta@arch-ws.localdomain>
2024-10-15 11:42:27 -0700

    eda.py updated for test traing split, and creating test set

---
+++ code/eda.py
@@ -0,0 +1,83 @@
+import numpy as np 
+import pandas as pd 
+
+# from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+
+housing = pd.read_csv("../datasets/housing.csv")
+
+# train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+# train_set, test_set = train_test_split(housing, test_size=0.2, random_state=42)
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+                               bins=[0., 1.5, 3.0, 4.5, 6., np.inf],
+                               labels=[1, 2, 3, 4, 5])
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+
+#############################################################################
+# training set 
+housing = strat_train_set.copy()
+housing = strat_train_set.drop("median_house_value", axis=1) # drop labels 
+housing_labels = strat_train_set["median_house_value"].copy()
+housing_num = housing.drop("ocean_proximity", axis=1)
+
+#############################################################################
+# column index
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room=True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+    def fit(self, X, y=None):
+        return self  # nothing else to do
+    def transform(self, X):
+        rooms_per_household = X[:, rooms_ix] / X[:, households_ix]
+        population_per_household = X[:, population_ix] / X[:, households_ix]
+        if self.add_bedrooms_per_room:
+            bedrooms_per_room = X[:, bedrooms_ix] / X[:, rooms_ix]
+            return np.c_[X, rooms_per_household, population_per_household,
+                         bedrooms_per_room]
+        else:
+            return np.c_[X, rooms_per_household, population_per_household]
+
+attr_adder = CombinedAttributesAdder(add_bedrooms_per_room=False)
+housing_extra_attribs = attr_adder.transform(housing.values)
+
+#############################################################################
+num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('attribs_adder', CombinedAttributesAdder()),
+        ('std_scaler', StandardScaler()),
+    ])
+
+# Alternatively: housing_num = housing.select_dtypes(include=[np.number])
+housing_num_tr = num_pipeline.fit_transform(housing_num)
+
+#############################################################################
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", OneHotEncoder(), cat_attribs),
+    ])
+housing_prepared = full_pipeline.fit_transform(housing)
+
+np.save("../datasets/housing_training_set_prepared.npy", housing_prepared)
+np.save("../datasets/housing_training_labels.npy", housing_labels)
+
+# Test Preapared
